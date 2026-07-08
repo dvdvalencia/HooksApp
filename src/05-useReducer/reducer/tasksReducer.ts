c68bb953e91b3bf01b620dda368db3768c78593a
DVDVALENCIA <dvdvalenciavasquez88@gmail.com>
2026-07-08 00:41:48 -0500

    Configuracion de la función Reducer  - tasksReducer.ts -

--- src/05-useReducer/reducer/tasksReducer.ts
+++ src/05-useReducer/reducer/tasksReducer.ts
@@ -20,5 +20,52 @@ export const tasksReducer = (
   state: TaskState,
   action: TaskAction,
 ): TaskState => {
-  return state;
+  switch (action.type) {
+    case "ADD_TODO": {
+      const newTodo: Todo = {
+        id: Date.now(),
+        text: action.payload,
+        completed: false,
+      };
+
+      return {
+        ...state,
+        todos: [...state.todos, newTodo],
+        length: state.todos.length + 1,
+        pending: state.pending + 1,
+      };
+    }
+
+    case "DELETE_TODO": {
+      const currentTodos = state.todos.filter(
+        (todo) => todo.id !== action.payload,
+      );
+      return {
+        ...state,
+        todos: currentTodos,
+        length: currentTodos.length,
+        completed: currentTodos.filter((todo) => todo.completed).length,
+        pending: currentTodos.filter((todo) => !todo.completed).length,
+      };
+    }
+
+    case "TOGGLE_TODO": {
+      const updatedTodos = state.todos.map((todo) => {
+        if (todo.id === action.payload) {
+          return { ...todo, completed: !todo.completed };
+        }
+        return todo;
+      });
+
+      return {
+        ...state,
+        todos: updatedTodos,
+        completed: updatedTodos.filter((todo) => todo.completed).length,
+        pending: updatedTodos.filter((todo) => !todo.completed).length,
+      };
+    }
+
+    default:
+      return state;
+  }
 };
