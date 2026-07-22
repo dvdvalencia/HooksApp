5c4491e3cf8af818b38b7b139354099ecafb6305
DVDVALENCIA <dvdvalenciavasquez88@gmail.com>
2026-07-22 16:03:15 -0500

    efectos secundarios con un reducer - Reducer - ScrambeWordsUseState.tsx - ScrambeWordsReducer.ts -

--- src/05-useReducer/reducer/ScrambeWordsUseState.tsx
+++ src/05-useReducer/reducer/ScrambeWordsUseState.tsx
@@ -2,7 +2,7 @@
 // Es necesario componentes de Shadcn/ui
 // https://ui.shadcn.com/docs/installation/vite
 
-import React, { useReducer, useState } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Card, CardContent } from "@/components/ui/card";
@@ -33,6 +33,15 @@ export const ScrambleWords = () => {
     skipCounter,
     totalWords,
   } = state;
+
+  useEffect(() => {
+    if (points === 0 ) return;
+    confetti({
+      particleCount: 100,
+      spread: 100,
+      origin: { y: 0.6 },
+    });
+  },[points])
 
   const handleGuessSubmit = (e: React.FormEvent) => {
     // Previene el refresh de la página
